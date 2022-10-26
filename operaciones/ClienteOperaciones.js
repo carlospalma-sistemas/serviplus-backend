5ec3ae7ae8fd2a2a1032492b9e767382113371bd
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2022-10-25 19:48:40 -0500

    Incorporados métodos PUT y GET id

--- operaciones/ClienteOperaciones.js
+++ operaciones/ClienteOperaciones.js
@@ -15,19 +15,65 @@ clienteOperaciones.crearCliente = async (req, res)=>{
 
 clienteOperaciones.buscarClientes = async (req, res)=>{
     try {
-        const listaclientes = await clienteModelo.find();
+        const filtro = req.query;
+        let listaclientes;
+        if (filtro.q != null) {
+            listaclientes = await clienteModelo.find({
+                "$or" : [ 
+                    { "nombres": { $regex:filtro.q, $options:"i" }},
+                    { "apellidos": { $regex:filtro.q, $options:"i" }},
+                    { "direccion": { $regex:filtro.q, $options:"i" }}
+                ]
+            });
+        }
+        else {
+            listaclientes = await clienteModelo.find(filtro);
+        }
         if (listaclientes.length > 0){
             res.status(200).send(listaclientes);
         } else {
             res.status(404).send("No hay datos");
         }
     } catch (error) {
-        res.status(400).send("Mala petición.");
+        res.status(400).send("Mala petición. "+error);
+    }
+}
+
+clienteOperaciones.buscarCliente = async (req, res)=>{
+    try {
+        const id = req.params.id;
+        const cliente = await clienteModelo.findById(id);
+        if (cliente != null){
+            res.status(200).send(cliente);
+        } else {
+            res.status(404).send("No hay datos");
+        }
+    } catch (error) {
+        res.status(400).send("Mala petición. "+error);
     }
 }
 
 clienteOperaciones.modificarCliente = async (req, res)=>{
-    
+    try {
+        const id = req.params.id;
+        const body = req.body;
+        const datosActualizar = {
+            nombres: body.nombres,
+            apellidos: body.apellidos,
+            direccion: body.direccion,
+            telefono: body.telefono,
+            passw: body.passw
+        }
+        const clienteActualizado = await clienteModelo.findByIdAndUpdate(id, datosActualizar, { new : true });
+        if (clienteActualizado != null) {
+            res.status(200).send(clienteActualizado);
+        }
+        else {
+            res.status(404).send("No hay datos");
+        }
+    } catch (error) {
+        res.status(400).send("Mala petición. "+error);
+    }
 }
 
 module.exports = clienteOperaciones;
--- rutas/ClienteRutas.js
+++ rutas/ClienteRutas.js
@@ -2,6 +2,8 @@ const clienteOperaciones = require("../operaciones/ClienteOperaciones");
 const router = require("express").Router();
 
 router.get("/", clienteOperaciones.buscarClientes);
+router.get("/:id", clienteOperaciones.buscarCliente);
 router.post("/", clienteOperaciones.crearCliente);
+router.put("/:id", clienteOperaciones.modificarCliente);
 
 module.exports = router;
